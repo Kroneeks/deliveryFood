19c1fbe9bb6e4342195a1777b3a4289ebfd51d9d
Sasha Donets <sashadoneze@gmail.com>
2022-07-26 21:01:23 +0300

    Create cart redux slice

---
+++ src/stores/cart/cartSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      return {
+        products: [...state.products, { ...action.payload, amount: 1 }],
+      };
+    },
+    clearCart: (state) => {
+      return { products: [] };
+    },
+    incrementProductAmount: (state, action) => {
+      return {
+        products: state.products.map((item) =>
+          item === action.payload.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        ),
+      };
+    },
+
+    decrementProductAmount: (state, action) => {
+      return {
+        products: state.products.map((item) =>
+          item === action.payload.id
+            ? { ...item, amount: item.amount - 1 }
+            : item
+        ),
+      };
+    },
+  },
+});
+
+export const cartProducts = (state) => state.cart.products;
+
+export const {
+  addToCart,
+  clearCart,
+  incrementProductAmount,
+  decrementProductAmount,
+} = cartSlice.actoins;
+
+export default cartSlice.reducer;

ade92de9341339ef4639d209880ffc7260377bf5
Sasha Donets <sashadoneze@gmail.com>
2022-07-27 20:21:36 +0300

    Create addToCart functionality

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import { ProductPreviewCard } from "./ProductPreviewCard";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../stores/cart/cartSlice";
 
 export const ProductsPreview = () => {
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   const responsive = {
     superLargeDesktop: {
@@ -33,14 +36,22 @@ export const ProductsPreview = () => {
       .catch((e) => console.log(e));
   }, []);
 
+  const onAddProduct = (product) => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <div className="container mx-auto pb-4 w-2/3">
       <Carousel responsive={responsive}>
         {products.length > 0 &&
-          products.map((item, _index) => {
+          products.map((item, index) => {
             return (
-              <div className="w-full p-3">
-                <ProductPreviewCard key={item._id} product={item} />
+              <div className="w-full p-3" key={item._id}>
+                <ProductPreviewCard
+                  key={item._id}
+                  product={item}
+                  onAddProduct={onAddProduct}
+                />
               </div>
             );
           })}
